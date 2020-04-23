d4e7ee6ffb51853e12c548d89a74c97346aee103
Brad Leege <bleege@gmail.com>
2020-04-22 21:02:28 -0500

    Adding logging

--- src/main/kotlin/io/leege/fcfinderv2/Application.kt
+++ src/main/kotlin/io/leege/fcfinderv2/Application.kt
@@ -1,5 +1,6 @@
 package io.leege.fcfinderv2
 
+import org.apache.log4j.PropertyConfigurator
 import spark.Spark.get
 import java.util.*
 
@@ -9,8 +10,18 @@ class Application {
 
         @JvmStatic
         fun main(args: Array<String>) {
+            initLogger()
             setupPort()
             setupRoutes()
+        }
+
+        private fun initLogger() {
+            val logProperties = Properties()
+            logProperties.setProperty("log4j.rootLogger", "DEBUG,CONSOLE")
+            logProperties.setProperty("log4j.appender.CONSOLE", "org.apache.log4j.ConsoleAppender")
+            logProperties.setProperty("log4j.appender.CONSOLE.layout", "org.apache.log4j.PatternLayout")
+            logProperties.setProperty("log4j.appender.CONSOLE.layout.conversionPattern", "%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%m%n")
+            PropertyConfigurator.configure(logProperties)
         }
 
         private fun setupPort(): Int {
