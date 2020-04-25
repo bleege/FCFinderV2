0d26f05c5437bb3af29d22ff0d9e362ab2797dcb
Brad Leege <bleege@gmail.com>
2020-04-25 16:43:02 -0500

    Setting port

--- src/main/kotlin/io/leege/fcfinderv2/Application.kt
+++ src/main/kotlin/io/leege/fcfinderv2/Application.kt
@@ -1,12 +1,15 @@
 package io.leege.fcfinderv2
 
 import org.apache.log4j.PropertyConfigurator
+import org.slf4j.LoggerFactory
 import spark.Spark.get
+import spark.Spark.port
 import java.util.*
 
 class Application {
 
     companion object {
+        val logger = LoggerFactory.getLogger(Application::class.java)
 
         @JvmStatic
         fun main(args: Array<String>) {
@@ -17,19 +20,19 @@ class Application {
 
         private fun initLogger() {
             val logProperties = Properties()
-            logProperties.setProperty("log4j.rootLogger", "DEBUG,CONSOLE")
+            logProperties.setProperty("log4j.rootLogger", "INFO,CONSOLE")
             logProperties.setProperty("log4j.appender.CONSOLE", "org.apache.log4j.ConsoleAppender")
             logProperties.setProperty("log4j.appender.CONSOLE.layout", "org.apache.log4j.PatternLayout")
             logProperties.setProperty("log4j.appender.CONSOLE.layout.conversionPattern", "%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%m%n")
             PropertyConfigurator.configure(logProperties)
         }
 
-        private fun setupPort(): Int {
+        private fun setupPort() {
             val processBuilder = ProcessBuilder()
-            if (processBuilder.environment().get("PORT") != null) {
-                return Integer.parseInt(processBuilder.environment().get("PORT"))
-            }
-            return 4567
+            val portEnvironment = processBuilder.environment().get("PORT")
+            val portInt = Integer.parseInt(portEnvironment ?: "4567")
+            logger.info("PORT set to $portInt")
+            port(portInt)
         }
 
         private fun setupRoutes() {
