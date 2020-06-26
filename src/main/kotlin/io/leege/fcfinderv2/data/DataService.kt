7a8d69f9e42a9f08bc449c3ee94086999ebea171
Brad Leege <bleege@gmail.com>
2020-06-25 22:11:59 -0500

    Converting to Schema Data Objects

--- src/main/kotlin/io/leege/fcfinderv2/data/DataService.kt
+++ src/main/kotlin/io/leege/fcfinderv2/data/DataService.kt
@@ -14,19 +14,19 @@ class DataService {
             user = "snetofsxfiajkf", password = "31d115e6cbd4a952c010c1b93becfc83b12443199e92935fba00c9ced622613e")
     }
 
-    fun getAllClubs(): List<Club> {
+    fun getAllClubs(): List<io.leege.fcfinderv2.schemas.models.Club> {
         return transaction {
             addLogger(Slf4jSqlDebugLogger)
-            Club.all().map { it }
+            Club.all().map { io.leege.fcfinderv2.schemas.models.Club(id = it.id.value, countryId = it.country.id, name = it.name, latitude = it.latitude, longitude = it.longitude ) }
 //            Clubs.selectAll().map { it.toClub() }
         }
     }
 
-    fun getAllCountries(): List<Country> {
+    fun getAllCountries(): List<io.leege.fcfinderv2.schemas.models.Country> {
 
         return transaction {
             addLogger(Slf4jSqlDebugLogger)
-            Country.all().map { it }
+            Country.all().map { io.leege.fcfinderv2.schemas.models.Country(id = it.id.value, name = it.name) }
 //            Countries.selectAll().map { it.toCountry() }
         }
     }
