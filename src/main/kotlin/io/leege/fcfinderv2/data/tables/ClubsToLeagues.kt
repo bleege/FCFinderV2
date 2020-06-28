92cc9ac1944a6d82f858886e8587252d5a12b248
Brad Leege <bleege@gmail.com>
2020-06-27 21:48:33 -0500

    Naming table

--- src/main/kotlin/io/leege/fcfinderv2/data/tables/ClubsToLeagues.kt
+++ src/main/kotlin/io/leege/fcfinderv2/data/tables/ClubsToLeagues.kt
@@ -2,7 +2,7 @@ package io.leege.fcfinderv2.data.tables
 
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-object ClubsToLeagues: IntIdTable() {
+object ClubsToLeagues: IntIdTable("clubs_to_leagues") {
 
     val clubId = ClubsToLeagues.integer("club_id").uniqueIndex().references(Clubs.id)
     val leagueId = ClubsToLeagues.integer("league_id").uniqueIndex().references(Leagues.id)
