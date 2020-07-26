eeffeaaa50a78cef92fdb9eb41fc07a34215563c
Brad Leege <bleege@gmail.com>
2020-07-26 16:27:16 -0500

    Adding Access Control Header Wildcard

--- src/main/kotlin/io/leege/fcfinderv2/Application.kt
+++ src/main/kotlin/io/leege/fcfinderv2/Application.kt
@@ -87,6 +87,7 @@ class Application {
             before("/graphql") { request, response ->
                 response.header("Access-Control-Allow-Origin", "*")
                 response.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+                response.header("Access-Control-Allow-Headers", "*")
             }
 
         }
